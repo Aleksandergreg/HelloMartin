7d7bea4118f1d108706d0a542746f650624dd5cc
maha0013 <maha0013@stud.kea.dk>
2023-09-25 09:03:02 +0200

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Martin");
-        System.out.println("Hello Aleksander");
+        System.out.println("Det bliver et godt projekt");
     }
 }
