46e38a2602a67d38823d25bd7a9c5eb410e892ef
Aleksandergreg <alek3691@stud.kea.dk>
2023-09-25 09:17:49 +0200

    Initial comnmit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Martin");
-        System.out.println("Det bliver et godt projekt");
+        System.out.println("Hello Aleksander");
     }
 }
