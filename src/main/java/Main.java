7c1a270b2530030ed7803b30de7d7b101de1933a
Aleksandergreg <alek3691@stud.kea.dk>
2023-09-25 09:19:41 +0200

    Initial comnmit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Martin");
         System.out.println("Hello Aleksander");
+        System.out.println("Hello 2");
+        System.out.println("Hello 1");
+        System.out.println("Hello 3");
+
     }
 }
